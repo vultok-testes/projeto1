6317ff4b6514010e5d78b744b82bab3f8fdede4b
Lab103 <Lab103@LAB101-18>
2017-09-22 16:07:45 -0300

    V1.003

--- project1/teste/br/sceweb/teste_unitario/UC01CadastrarEmpresa.java
+++ project1/teste/br/sceweb/teste_unitario/UC01CadastrarEmpresa.java
@@ -12,23 +12,34 @@ import br.sceweb.model.EmpresaDAO;
 public class UC01CadastrarEmpresa {
 	public static Empresa empresa;
 	public static EmpresaDAO empresaDAO;
-	
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		empresa = new Empresa();
 		empresaDAO = new EmpresaDAO();
+		empresa = new Empresa();
+		empresa.setNomeDaEmpresa("empresa x");
+		empresa.setCnpj("89424232000180");
+		empresa.setNomeFantasia("empresa x");
+		empresa.setEndereco("rua taquari");
+		empresa.setTelefone("2222");
+	}
+
+	@Test
+	public void CT01UC01FBCadastra_com_sucesso() {
+		assertEquals(1, empresaDAO.adiciona(empresa));
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		empresaDAO.exclui(empresa.getCnpj());
 	}
 
 	@Test
 	public void CT01UC01CadastrarEmpresa_com_sucesso() {
 		assertEquals(1, empresaDAO.adiciona(empresa));
 	}
-	
-	@Test(expected=IllegalArgumentException.class)
+
+	@Test(expected = IllegalArgumentException.class)
 	public void CT02UC01CadastrarEmpresa_com_nome_invalido() {
 		empresa.setNomeDaEmpresa("");
 	}
